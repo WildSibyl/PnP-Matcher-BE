11cb91124c6ce553391501bf972c449675b27342
LukasF7889 <lukas_fritsch@hotmail.de>
2025-05-30 15:52:24 +0200

    Adjusted Rollforgroup

--- controllers/users.js
+++ controllers/users.js
@@ -432,14 +432,19 @@ export const getRollMatches = async (req, res) => {
 
       const matchOver80 = userWithScore.filter((e) => e.matchScore > 80);
       const matchOver70 = userWithScore.filter((e) => e.matchScore > 70);
+      const matchOver10 = userWithScore.filter((e) => e.matchScore > 10);
       let randomFour;
-      if (matchOver80.length > 8) {
+      if (matchOver80.length > 5) {
         //give back 4 random users with match score over 80
         const shuffled = matchOver80.sort(() => 0.5 - Math.random());
         randomFour = shuffled.slice(0, 4);
-      } else if (matchOver70.length > 8) {
+      } else if (matchOver70.length > 5) {
         //give back 4 random users with match score over 70
         const shuffled = matchOver70.sort(() => 0.5 - Math.random());
+        randomFour = shuffled.slice(0, 4);
+      } else if (matchOver10.length > 5) {
+        //give back 4 random users with match score over 10
+        const shuffled = matchOver10.sort(() => 0.5 - Math.random());
         randomFour = shuffled.slice(0, 4);
       } else {
         return res.status(404).json({ message: "Not enough users nearby" });
