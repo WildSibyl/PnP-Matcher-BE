1c18ef0848a5be1d1c02aad62b6bf1059642ed7a
LukasF7889 <lukas_fritsch@hotmail.de>
2025-06-05 12:14:13 +0200

    Fallback if user has no coordinates

--- controllers/groups.js
+++ controllers/groups.js
@@ -199,7 +199,7 @@ export const getFilteredGroups = async (req, res) => {
     }
 
     //User groups or empty array (if no user is logged in)
-    const userGroupIds = currentUser.groups || [];
+    const userGroupIds = currentUser?.groups || [];
 
     //filter groups out if they have the user as member or if they are in the groups list
     query._id = { $nin: userGroupIds };
@@ -259,7 +259,7 @@ export const getFilteredGroups = async (req, res) => {
 
     //CalculateMatchScore
     const groupsWithScore = groups.map((group) => {
-      if (currentUser) {
+      if (currentUser && currentUser.address?.location?.coordinates) {
         const userCoords = currentUser.address?.location?.coordinates;
         const groupCoords = group.address?.location?.coordinates;
 
