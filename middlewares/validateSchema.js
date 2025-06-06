e7c7d20124088dea25986d7e8c9f87b47dd0fb72
LukasF7889 <lukas_fritsch@hotmail.de>
2025-06-06 15:30:48 +0200

    Default image for groups

--- middlewares/validateSchema.js
+++ middlewares/validateSchema.js
@@ -3,11 +3,13 @@ import ErrorResponse from "../utils/ErrorResponse.js";
 const validateSchema = (joiSchema) => (req, res, next) => {
   const { error } = joiSchema.validate(req.body);
 
-  if (error) console.log("Validation error: ", error.details);
-  throw new ErrorResponse(
-    "Some fields have not been filled correctly, please check!",
-    400
-  );
+  if (error) {
+    console.log("Validation error: ", error.details);
+    throw new ErrorResponse(
+      "Some fields have not been filled correctly, please check!",
+      400
+    );
+  }
 
   next();
 };
